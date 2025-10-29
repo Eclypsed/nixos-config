c3eb30b199c96b1404f13d9190616987b5f207e1
Eclypsed <Ec1ypsed@proton.me>
2025-10-28 23:55:12 -0400

    Added Alacritty

---
+++ modules/home/alacritty.nix
@@ -0,0 +1,17 @@
+{
+  ...
+}:
+{
+  programs.alacritty = {
+    enable = true;
+    theme = "catppuccin_mocha";
+    settings = {
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+        };
+      };
+    };
+  };
+}
